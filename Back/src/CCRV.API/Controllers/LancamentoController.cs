b721a7f5c0d4f3b03a0d7f06de9baf17fdbc1f0f
rogeriojfa <rogeirojfa@gmail.com>
2025-01-16 15:08:38 -0300

    Introdução ao EF Core

--- Back/src/CCRV.API/Controllers/LancamentoController.cs
+++ Back/src/CCRV.API/Controllers/LancamentoController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CCRV.API.Data;
 using CCRV.API.Models;
 using Microsoft.AspNetCore.Http.Connections;
 using Microsoft.AspNetCore.Mvc;
@@ -14,38 +15,23 @@ namespace CCRV.API.Controllers
     public class LancamentoController : ControllerBase
     {
 
-        private IEnumerable<Lancamento> _lancamentos = new Lancamento[]{
-            new(){
-                Id = 1,
-                DataLancamento = DateTime.Now,
-                Descricao = "Dizimo/Oferta",
-                TipoLancamento = 1,
-                Valor = 890
-                },
-                new(){
-                Id = 2,
-                DataLancamento = DateTime.Now,
-                Descricao = "Aluguel Templo",
-                TipoLancamento = 2,
-                Valor = 700
-                }
-        };
+        public DataContext Context { get; }
 
-        public LancamentoController()
+        public LancamentoController(DataContext context)
         {
-
+            Context = context;
         }
 
         [HttpGet]
         public IEnumerable<Lancamento> Get()
         {
-            return _lancamentos;
+            return Context.Lancamentos;
         }
 
         [HttpGet("{id}")]
         public Lancamento GetById(int id)
         {
-            return _lancamentos.Where(l => l.Id == id).First();
+            return Context.Lancamentos.Where(l => l.Id == id).First();
         }
         [HttpPost]
         public string Post()
---
+++ Back/src/CCRV.API/Data/Migrations/20250116150247_initial.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CCRV.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Lancamentos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DataLancamento = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Descricao = table.Column<string>(type: "TEXT", nullable: true),
+                    TipoLancamento = table.Column<int>(type: "INTEGER", nullable: false),
+                    Valor = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lancamentos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Lancamentos");
+        }
+    }
+}
